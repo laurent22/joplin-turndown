4815da07dbde12caf85ed1f7277575bdcdf814a8
Laurent Cozic <laurent@cozic.net>
2020-12-02 13:05:42 +0000

    Make build faster for Node

--- build_for_test.sh
+++ build_for_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# ROOT_DIR=/mnt/c/Users/laurent/src/joplin
-ROOT_DIR=/Users/laurent/src/joplin
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-npm run build
-# rsync -a ./dist/ $ROOT_DIR/ElectronClient/app/node_modules/joplin-turndown/dist/
-# rsync -a ./lib/ $ROOT_DIR/ElectronClient/app/node_modules/joplin-turndown/lib/
-rm -rf $ROOT_DIR/CliClient/node_modules/joplin-turndown
-ln -s "$CURRENT_DIR" $ROOT_DIR/CliClient/node_modules/joplin-turndown
-cd $ROOT_DIR/CliClient && npm run test -- --filter=HtmlToMd
+# ROOT_DIR=/mnt/c/Users/laurent/src/joplin
+# ROOT_DIR=/Users/laurent/src/joplin
+ROOT_DIR=/home/laurent/source/joplin
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+npm run build-for-node
+rm -rf $ROOT_DIR/packages/lib/node_modules/joplin-turndown
+ln -s "$CURRENT_DIR" $ROOT_DIR/packages/lib/node_modules/joplin-turndown
+cd $ROOT_DIR/packages/app-cli && npm run test -- HtmlToMd
